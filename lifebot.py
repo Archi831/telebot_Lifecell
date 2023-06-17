caec8517d2298f06c3f44fc611223c2002a62e32
Ishaaq Ollite <iollite@hotmail.com>
2023-06-17 12:39:48 +0100

    Script now saves user data

--- lifebot.py
+++ lifebot.py
@@ -7,62 +7,78 @@ from telebot import types
 TOKEN = "5844896817:AAFqEWkdO27X6w13h5bzOBxFYhgJ58MBr80"
 bot = telebot.TeleBot(TOKEN)
 
-data = {"user" : [], "minutes" : [], "gigabytes" : []}
+# Load the data from the json file
+with open('data.json', 'r', encoding='utf-8') as file:
+    data = json.load(file)
+
 
 # Welcoming words
 @bot.message_handler(commands=['start'])
 def start_handler(message):
+    global data
     bot.send_message(message.chat.id, "Hello, I'm your personal advisor")
     print(message.chat.id)
-    data['user'].append(message.chat.id)
+    
 
 @bot.message_handler(commands=['find'])
 def finder(message):
+    global data
     chat_id = message.chat.id
+
+    # Check if the user is new
+    # If they are new, add them to the "database"
+    try:
+        data["users"][message.chat.id]
+        print("User found")
+    except KeyError:
+        print("New user")
+        data["users"][message.chat.id] = { "minutes": None, "gigabytes": None }
+
     bot.send_message(chat_id, 'I will help you to find your optimal tariff plan that best suits your needs.')
     time.sleep(1)
     bot.send_message(chat_id, 'First, you have to answer some questions')
     time.sleep(1)
     bot.send_message(chat_id, 'How often do you call other people?')
-    msg = bot.send_message(chat_id, 'Tell me how much do you talk (mins)')
-    bot.register_next_step_handler(msg, InputAnswer)
 
-def InputAnswer(message):
+    msg = bot.send_message(chat_id, 'Tell me how much do you talk (mins)')
+    bot.register_next_step_handler(msg, Input_user_minutes)
+
+def Input_user_minutes(message):
     global data
     text = message.text
     chat_id = message.chat.id
+
     if not message.text.isdigit():
         msg = bot.send_message(chat_id, 'please input a number')
-        bot.register_next_step_handler(msg, InputAnswer)
+        bot.register_next_step_handler(msg, Input_user_minutes)
         return
+    
     bot.send_message(chat_id, 'You speak for ' + str(text) + " minutes")
-    data['minutes'].append(text)
+    data["users"][message.chat.id]["minutes"] = text
     
 
     print(f"User {message.chat.id} talks for {text} minutes")
     msg = bot.send_message(chat_id, 'Tell me how much do you use internet (Gigabites)')
-    bot.register_next_step_handler(msg, InputAnswer2)
+    bot.register_next_step_handler(msg, Input_user_gigabites)
 
-def InputAnswer2(message):
-    global gigabytes
+def Input_user_gigabites(message):
+    global data
     text = message.text
     chat_id = message.chat.id
+
     if not message.text.isdigit():
         msg = bot.send_message(chat_id, 'please input a number')
-        bot.register_next_step_handler(msg, InputAnswer)
+        bot.register_next_step_handler(msg, Input_user_minutes)
         return
-    data['gigabytes'].append(text)
+    
+    data["users"][message.chat.id]["gigabytes"] = text
     print(f"User {message.chat.id} uses {text} gb of internet")
     bot.send_message(chat_id, 'You use ' + str(text) + " gigabytes of internet")
     msg = bot.send_message(chat_id, "Saving your data...")
-    bot.register_next_step_handler(msg, write_json)
-
-def write_json(new_data, filename='data.json'):
-    with open(filename,'r+', encoding='utf-8') as file:
-        file_data = json.load(file)
-        file_data["users"].append(new_data)
-        file.seek(0)
-        json.dump(file_data, file, indent = 4)
+    
+    with open('data.json', 'w', encoding='utf-8') as file:
+        json.dump(data, file, indent=4, ensure_ascii=False)
+        
 
 def tariff_choser():
     pass
